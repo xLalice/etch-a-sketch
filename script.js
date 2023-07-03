0336f177a7997310f14040e30e993a131dd3cf4c
xLalice <dreamsend08@gmail.com>
2023-07-03 19:59:11 +0800

    Create 16x16 grid

---
+++ script.js
@@ -0,0 +1,12 @@
+for (let i = 0; i < 16; i++){
+    let container = document.querySelector(".container");
+    let row = document.createElement("div");
+    row.classList.add("row");
+
+    for (let j = 0; j < 16; j++){
+        let divSquare = document.createElement("div");
+        divSquare.classList.add("cell");
+        row.appendChild(divSquare);
+    }
+    container.appendChild(row);
+}
